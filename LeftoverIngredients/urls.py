d64d1b45ed3521ccfbfeb0bc968240b61aeff0dd
Jinho(David) Seo <brownkpt60@gmail.com>
2021-12-02 15:31:59 -0800

    test revert

--- LeftoverIngredients/urls.py
+++ LeftoverIngredients/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
-
 from users import views as user_views
 
 urlpatterns = [
